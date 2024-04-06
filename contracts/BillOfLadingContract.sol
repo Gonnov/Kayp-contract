8ddae31c5b23f3dcae0b0d96b3171ca19138b574
neoff69 <76119301+neoff69@users.noreply.github.com>
2024-04-06 13:03:21 +0200

    add of exepition date

--- contracts/BillOfLadingContract.sol
+++ contracts/BillOfLadingContract.sol
@@ -4,12 +4,17 @@ pragma solidity ^0.8.24;
 
 contract BillOfLadingContract {
     bytes32 hashedBillOfLadingData;
+    uint256 lastUpdated;
+    uint256 expeditionDate;
     address owner;
 
     constructor (
-        string memory _rawBillOfLadingData
+        string memory _rawBillOfLadingData,
+        uint256 _expeditionDate
     ) {
         hashedBillOfLadingData = keccak256(abi.encodePacked(_rawBillOfLadingData));
+        lastUpdated = block.timestamp;
+        expeditionDate = _expeditionDate;
         owner = msg.sender;
     }
 
@@ -18,14 +23,20 @@ contract BillOfLadingContract {
 		_;
 	}
 
+    function setExpeditionDate(uint256 _expeditionDate) external onlyOwner {
+        require(block.timestamp < expeditionDate, "Expedition date has passed");
+        expeditionDate = _expeditionDate;
+    }
+
     function setBillOfLadingData(string memory _rawBillOfLadingData) external onlyOwner {
+        require(block.timestamp < expeditionDate, "Expedition date has passed");
         hashedBillOfLadingData = keccak256(abi.encodePacked(_rawBillOfLadingData));
+        lastUpdated = block.timestamp;
     }
 
     function checkBillOfLadingData(string memory _rawBillOfLadingData) external onlyOwner view returns (bool) {
         return keccak256(abi.encodePacked(_rawBillOfLadingData)) == hashedBillOfLadingData;
     }
-
 }
 
 
--- test/ContractTest.ts
+++ test/ContractTest.ts
@@ -1,26 +1,62 @@
-import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { ethers } from "hardhat";
 import { expect } from "chai";
-const { ethers } = require("hardhat");
-
-import "@nomicfoundation/hardhat-ethers";
+import { Contract, Signer } from "ethers";
 
 describe("BillOfLadingContract", function () {
-    const _rawBillOfLadingInfo = "test";
-    async function billOfLadingContractFixture() {
-        const [owner, addr1, addr2] = await ethers.getSigners();
-        const billOfLadingContract = await ethers.deployContract(
-            "BillOfLadingContract",
-            [_rawBillOfLadingInfo]
+    let billOfLadingContract: Contract;
+    let owner: string;
+    const rawBillOfLadingData: string = "test";
+    const expeditionDate: number = Math.floor(Date.now() / 1000) + 86400; // Tomorrow
+
+    beforeEach(async function () {
+        const signers: Signer[] = await ethers.getSigners();
+        owner = await signers[0].getAddress();
+
+        const BillOfLadingContract = await ethers.getContractFactory(
+            "BillOfLadingContract"
         );
-        await billOfLadingContract.waitForDeployment();
-        return { billOfLadingContract, owner, addr1, addr2 };
-    }
-    describe("Deployment", function () {
-        it("Should set the right owner", async function () {
-            const { billOfLadingContract, owner } = await loadFixture(
-                billOfLadingContractFixture
-            );
-            expect(await billOfLadingContract.owner()).to.equal(owner.address);
-        });
+        billOfLadingContract = await BillOfLadingContract.deploy(
+            rawBillOfLadingData,
+            expeditionDate
+        );
     });
-});
+
+    it("Should return true when checking bill of lading data", async function () {
+        const result = await billOfLadingContract.checkBillOfLadingData(
+            rawBillOfLadingData
+        );
+        expect(result).to.be.true;
+    });
+
+    it("Should return false when checking incorrect bill of lading data", async function () {
+        const result = await billOfLadingContract.checkBillOfLadingData(
+            "incorrect data"
+        );
+        expect(result).to.be.false;
+    });
+
+    it("Set a new Bill of lading data and check it with the new one", async function () {
+        await billOfLadingContract.setBillOfLadingData("new data");
+        const result = await billOfLadingContract.checkBillOfLadingData(
+            "new data"
+        );
+        expect(result).to.be.true;
+    });
+
+    it("Set a new Bill of lading data and check it with the old one", async function () {
+        await billOfLadingContract.setBillOfLadingData("new data");
+        const result = await billOfLadingContract.checkBillOfLadingData("test");
+        expect(result).to.be.false;
+    });
+
+    it("Should not allow non-owner to set bill of lading data", async function () {
+        const nonOwner = (await ethers.getSigners())[1];
+        await expect(
+            billOfLadingContract
+                .connect(nonOwner)
+                .setBillOfLadingData("new data")
+        ).to.be.revertedWith("Not the owner");
+    });
+
+   
+
